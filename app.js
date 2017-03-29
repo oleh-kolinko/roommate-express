04a22ccb9e59f9828fa7a4cca591c3b420b88288
Oleh Kolinko <oleh.kolinko@gmail.com>
2017-03-29 12:54:55 -0400

    prod v1.0

--- app.js
+++ app.js
@@ -73,8 +73,7 @@ app.use('/api', api);
 app.use(ensure.ensureLoggedIn());
 
 app.use((req,res,next)=>{
-  res.send('angular');
-  // res.sendfile(__dirname + '/public/index.html');
+  res.sendfile(__dirname + '/public/index.html');
 });
 
 // catch 404 and forward to error handler
